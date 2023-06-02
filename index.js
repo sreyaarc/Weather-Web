2eddbe4a3b1adfd15e13db16d77e1388897f058e
sreyaarc <sreya66nambiar@gmail.com>
2023-06-02 06:52:25 +0530

    final informations without styling

---
+++ index.js
@@ -0,0 +1,47 @@
+import * as dotenv from "dotenv";
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import path from "path";
+import https from "https";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
+dotenv.config({path: path.resolve(__dirname+"/.env")});
+
+import express from "express";
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/index.html")
+})
+
+const apiKey = process.env.API_KEY;
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+app.post("/", (req, res) => {
+    // const url = baseUrl+"q="+req.body.inputCity+"&appid="+apiKey+"&units=metric";
+    const url = `${baseUrl}q=${req.body.inputCity}&appid=${apiKey}&units=metric`
+    https.get(url, (response) => {
+        response.on("data", (d) => {
+            const parsedData = JSON.parse(d);
+            // res.send(parsedData);
+            const imgURL = `https://openweathermap.org/img/wn/${parsedData.weather[0].icon}@2x.png`
+            res.write(`<h1>${parsedData.name}, ${parsedData.sys.country}</h1>` );
+            res.write(`<div><img src=${imgURL}></div>`);
+            res.write(`<div>Today's weather: ${parsedData.weather[0].main} - ${parsedData.weather[0].description}</div>`);
+            res.write(`<div>Temperature: ${parsedData.main.temp}&#8451;</div>`);
+            
+            res.send();
+        })
+    })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server started running at "+port)
+})
